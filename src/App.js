08533a318665c2a5b0beb6ec9ada2336c904a3b4
chubi-x <chubixax@protonmail.com>
2022-07-25 19:51:50 +0100

    feat: pass card props from app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import Header from "./Components/Header";
 import Card from "./Components/Card";
+import data from "./Data/data";
 function App() {
   return (
     <div className="App">
-      <Header></Header>
+      <Header />
       <main>
-        <Card></Card>
+        {data.map((item) => (
+          <Card key={item.id} item={item} />
+        ))}
       </main>
     </div>
   );
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import pin from "../images/pin.png";
-export default function Card() {
+export default function Card(props) {
+  const item = props.item;
+
   return (
     <section className="card">
-      <div className="card-image"></div>
+      <div className="card-image">
+        <img src={item.img} alt="card" />
+      </div>
       <div className="card-details">
         <div className="card-location">
           <img src={pin} alt="location pin" />
+          <p>{item.location}</p>
         </div>
-        <div className="card-title"></div>
-        <div className="card-date"></div>
-        <div className="card-description"></div>
+        <div className="card-title">{item.title}</div>
+        <div className="card-date">{item.date}</div>
+        <div className="card-description">{item.description}</div>
       </div>
     </section>
   );
