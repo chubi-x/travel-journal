e3f2bcb30423fc22dcfac50a77558052633dd08b
chubi-x <chubixax@protonmail.com>
2022-07-25 19:49:46 +0100

    chore: assign data id

--- src/Data/data.js
+++ src/Data/data.js
@@ -1,5 +1,5 @@
-export
 const data = [{
+        id: 1,
         img: "https://source.unsplash.com/WLxQvbMyfas",
         location: 'JAPAN',
         title: "Mount Fuji",
@@ -7,6 +7,7 @@ const data = [{
         description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
     },
     {
+        id: 2,
         img: "https://source.unsplash.com/JmuyB_LibRo",
         location: 'AUSTRALIA',
         title: "Sydney Opera House",
@@ -14,6 +15,7 @@ const data = [{
         description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
     },
     {
+        id: 3,
         img: "https://source.unsplash.com/3PeSjpLVtLg",
         location: 'NORWAY',
         title: "Geirangerfjord",
@@ -21,3 +23,4 @@ const data = [{
         description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
     }
 ]
+export default data;
