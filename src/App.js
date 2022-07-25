1177c833fc7f15f479061e499a82649482bda793
chubi-x <chubixax@protonmail.com>
2022-07-25 19:37:59 +0100

    feat: import card component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
 import Header from "./Components/Header";
+import Card from "./Components/Card";
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <main>
+        <Card></Card>
+      </main>
     </div>
   );
 }
