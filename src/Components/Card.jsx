5a3bd1d2ab716c6589d79e62f049fbae16de4466
chubi-x <chubixax@protonmail.com>
2022-07-26 11:36:40 +0100

    chore: card html and styling

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -4,19 +4,27 @@ export default function Card(props) {
   const item = props.item;
 
   return (
-    <section className="card">
-      <div className="card-image">
-        <img src={item.img} alt="card" />
-      </div>
-      <div className="card-details">
-        <div className="card-location">
-          <img src={pin} alt="location pin" />
-          <p>{item.location}</p>
+    <>
+      <section className="card">
+        <div className="card-image">
+          <img src={item.img} alt="card" />
         </div>
-        <div className="card-title">{item.title}</div>
-        <div className="card-date">{item.date}</div>
-        <div className="card-description">{item.description}</div>
-      </div>
-    </section>
+        <div className="card-details">
+          <div className="card-location">
+            <img src={pin} alt="location pin" />
+            <span>{item.location}</span>
+            <a href={item.mapLink}>View on Google Maps</a>
+          </div>
+          <div className="card-title">
+            <p>{item.title}</p>
+          </div>
+          <div className="card-date">
+            <p>{item.date}</p>
+          </div>
+          <div className="card-description">{item.description}</div>
+        </div>
+      </section>
+      <hr style={{ width: "600px", border: "1px solid #F5F5F5" }} />
+    </>
   );
 }
